49a0fdffabb85fbaccef56e8bd2a44c89bc813ce
Justin Abrahms <justin@abrah.ms>
2024-02-05 10:52:01 -0800

    Handle slicing around utf-8 boundaries.

--- tests/file_reading_test.py
+++ tests/file_reading_test.py
@@ -1,12 +1,12 @@
 import io
 from cribl_python_interview.main import read_file_in_reverse
+import os.path
 
 def test_simple_file():
-    f = io.StringIO("line1\nline2")
+    f = io.BytesIO(b"line1\nline2")
     result = read_file_in_reverse(f)
     assert list(result) == ["line2", "line1"]
 
 def test_utf8():
-    f = io.StringIO("😀\n🫠")
-    result = read_file_in_reverse(f, chunk_size=1)
-    assert list(result) == ["🫠","😀"]
+    with open(os.path.join(os.path.dirname(__file__), 'UTF-8-demo.txt'), 'rb') as f:
+        assert next(read_file_in_reverse(f, chunk_size=20)) != ""
